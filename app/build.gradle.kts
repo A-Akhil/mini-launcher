9c0bb3edf20850ca0d2800104d0d74313e334797
A-Akhil <akhilrahul70@gmail.com>
2026-03-15 14:20:58 +0530

    chore: bump version to 2.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.minifocus.launcher"
     minSdk = 29
     targetSdk = 35
-    versionCode = 13
-    versionName = "2.1.0"
+    versionCode = 14
+    versionName = "2.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
