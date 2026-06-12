9ae2cb6662dd04f0bbb9f194aaebbdd792227da8
A-Akhil <akhilrahul70@gmail.com>
2026-06-12 19:59:49 +0530

    chore: bump version to 2.5.0 for calendar feature release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.minifocus.launcher"
     minSdk = 29
     targetSdk = 35
-    versionCode = 17
-    versionName = "2.4.0"
+    versionCode = 18
+    versionName = "2.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
